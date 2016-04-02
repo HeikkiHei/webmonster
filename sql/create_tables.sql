f68e7e8ce23c5911bd6f96b54dfa10cfa1db1502
HeikkiHei <heikkihei.ahonen@gmail.com>
2016-04-02 16:43:54 +0300

    sql edit3

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -2,22 +2,7 @@
 CREATE TABLE Gamemaster (
 id SERIAL PRIMARY KEY,
 name varhchar(50) NOT NULL,
-password varhchar(50)
-);
-
-CREATE TABLE Creature (
-id SERIAL PRIMARY KEY,
-name varchar(50) REFERENCES Name(name) NOT NULL,
-creatureClass varchar(50) REFERENCES CreatureClass(name) NOT NULL,
-race varchar(50) REFERENCES Race(name) NOT NULL,
-weapon varchar(50) REFERENCES Weapon(name) NOT NULL,
-strength int NOT NULL,
-dexterity int NOT NULL,
-constitution int NOT NULL,
-intelligence int NOT NULL,
-wisdom int NOT NULL,
-charisma int NOT NULL,
-
+password varchar(50)
 );
 
 CREATE TABLE Name (
@@ -44,4 +29,18 @@ name varchar(50) NOT NULL,
 description varchar(200) NOT NULL,
 minDamage int NOT NULL,
 maxDamage int NOT NULL
+);
+
+CREATE TABLE Creature (
+id SERIAL PRIMARY KEY,
+name varchar(50) REFERENCES Name(name) NOT NULL,
+creatureClass varchar(50) REFERENCES CreatureClass(name) NOT NULL,
+race varchar(50) REFERENCES Race(name) NOT NULL,
+weapon varchar(50) REFERENCES Weapon(name) NOT NULL,
+strength int NOT NULL,
+dexterity int NOT NULL,
+constitution int NOT NULL,
+intelligence int NOT NULL,
+wisdom int NOT NULL,
+charisma int NOT NULL
 );
