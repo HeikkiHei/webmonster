1a6348169e7109627f52993b321e5dcc24cf4d2d
HeikkiHei <heikkihei.ahonen@gmail.com>
2016-05-07 23:34:35 +0300

    dadaa

--- app/controllers/site_controller.php
+++ app/controllers/site_controller.php
@@ -10,7 +10,8 @@ class SiteController extends BaseController{
 		self::check_logged_in();
 		$gamemaster_id = self::get_gamemaster_id();
 		$gamemaster_logged_in = self::get_gamemaster_logged_in();
-		View::make('sitemap.html', array('gamemaster_logged_in' => $gamemaster_logged_in, 'gamemaster_id' => $gamemaster_id));
+		$moderator = Gamemaster::checkModerator($gamemaster_id);
+		View::make('sitemap.html', array('gamemaster_logged_in' => $gamemaster_logged_in, 'gamemaster_id' => $gamemaster_id, 'moderator' => $moderator));
 	}
 
 }
