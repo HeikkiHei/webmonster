f23963fa05a13f9df9d642b587c4297b14595404
HeikkiHei <heikkihei.ahonen@gmail.com>
2016-05-07 23:38:34 +0300

    fix stupid

--- lib/base_model.php
+++ lib/base_model.php
@@ -4,6 +4,18 @@ class BaseModel{
     // "protected"-attribuutti on käytössä vain luokan ja sen perivien luokkien sisällä
   protected $validators;
 
+  public function __construct($attributes = null){
+      // Käydään assosiaatiolistan avaimet läpi
+    foreach($attributes as $attribute => $value){
+        // Jos avaimen niminen attribuutti on olemassa...
+      if(property_exists($this, $attribute)){
+          // ... lisätään avaimen nimiseen attribuuttin siihen liittyvä arvo
+        $this->{$attribute} = $value;
+      }
+    }
+  }
+
+
   public function validate_name() {
     $errors = array();
     if($this->name == '' || $this->name == null) {
